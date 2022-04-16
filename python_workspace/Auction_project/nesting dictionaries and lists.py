759b8ce0bd2038a57b3035631f34a6e931d99441
lloydtonykatila <101291544+tonylloyd2@users.noreply.github.com>
2022-04-16 17:41:33 +0300

    Update nesting dictionaries and lists.py

---
+++ python_workspace/Auction_project/nesting dictionaries and lists.py
@@ -0,0 +1,16 @@
+capitals = {
+    "Kenya":"Nairobi",
+    "Germany":"berlin"
+}
+# nesting list in Dictionary
+travel_log = {
+
+    "Kenya":["Bungoma"," Migori"," Sondu"],
+    "Germany":["Dortmund ","Leverkusen","Wolsburg",]
+}
+# nesting a dictionary in a dictionary
+travel_log2 = {
+
+    "Kenya": {"cities_visited" : ["Bungoma"," Migori"," Sondu"] , total_visits },
+    "Germany": {"cities_visited" ["Dortmund ","Leverkusen","Wolsburg",]}
+}
