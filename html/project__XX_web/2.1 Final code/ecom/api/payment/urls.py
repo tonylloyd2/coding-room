e710da322aeb9785cd62d03106df8e57c461314c
lloydtonykatila <101291544+tonylloyd2@users.noreply.github.com>
2022-04-17 03:37:41 +0300

    deleting redundancy

--- html/project__XX_web/2.1 Final code/ecom/api/payment/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from . import views
-
-urlpatterns = [
-    path('gettoken/<str:id>/<str:token>/',
-         views.generate_token, name="token.generate"),
-    path('process/<str:id>/<str:token>/',
-         views.process_payment, name="payment.process"),
-]
